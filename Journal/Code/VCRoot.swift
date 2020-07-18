04d777d1e3828eab8798cf77399da34da2095085
asukim2020 <asukim2020@gmail.com>
2020-07-18 13:04:33 +0900

    first commit

---
+++ Journal/Code/VCRoot.swift
@@ -0,0 +1,27 @@
+//
+//  VCRoot.swift
+//  Journal
+//
+//  Created by Asu on 2020/07/18.
+//  Copyright © 2020 Asu. All rights reserved.
+//
+
+import UIKit
+
+class VCRoot: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    func setupUI() {
+        
+    }
+    
+    func displayUI() {
+        
+    }
+    
+}
